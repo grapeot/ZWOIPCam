3d75dca6b89f88d840b5ada47ce178cd7db47074
Yan Wang <grapeot@outlook.com>
2021-09-16 20:55:34 -0700

    Add some features.

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ logger.setLevel(logging.DEBUG)
 consoleHandler = logging.StreamHandler(sys.stderr)
 consoleHandler.setLevel(logging.DEBUG)
 consoleHandler.setFormatter(formatter)
-fileHandler = logging.handlers.RotatingFileHandler(filename="error.log",maxBytes=1024000, backupCount=10, mode="a")
+fileHandler = logging.handlers.RotatingFileHandler(filename="/home/pi/code/allsky/error.log",maxBytes=1024000, backupCount=10, mode="a")
 fileHandler.setLevel(logging.INFO)
 fileHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
@@ -60,9 +60,12 @@ class FileSaver(Thread):
 
 """The worker thread doing the heavy lifting"""
 class CameraCapture(Thread):
-    def __init__(self, output_stream, latest_stream):
+    def __init__(self, output_stream, latest_stream, interval=0):
         super(CameraCapture, self).__init__()
         self.terminate = False
+        self.interval = interval
+        self.last_gain = 0
+        self.last_exposure = 0
         self.initialize_camera()
 
         logger.info('Camera initialization complete.')
@@ -72,6 +75,7 @@ class CameraCapture(Thread):
 
     def initialize_camera(self):
         logger.info('Initializing camera...')
+        sleep(2)
         num_cameras = asi.get_num_cameras()
         if num_cameras == 0:
             raise RuntimeError('No ZWO camera was detected.')
@@ -81,17 +85,22 @@ class CameraCapture(Thread):
         camera_info = self.camera.get_camera_property()
         logger.info(camera_info)
         controls = self.camera.get_controls()
+        logger.info(controls)
 
         if camera_info['IsColorCam']:
             self.camera.set_image_type(asi.ASI_IMG_RGB24)
         else:
             self.camera.set_image_type(asi.ASI_IMG_RAW8)
 
+        self.camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, 
+                                self.camera.get_controls()['BandWidth']['DefaultValue'],
+                                auto=True)
+
         # Set auto exposure value
         self.whbi = self.camera.get_roi_format()
         self.camera.set_control_value(asi.ASI_WB_B, 99)
         self.camera.set_control_value(asi.ASI_WB_R, 75)
-        self.camera.set_control_value(asi.ASI_AUTO_MAX_GAIN, 75)
+        self.camera.set_control_value(asi.ASI_AUTO_MAX_GAIN, 100)
         self.camera.set_control_value(asi.ASI_AUTO_MAX_BRIGHTNESS, 160)
         self.camera.set_control_value(asi.ASI_EXPOSURE,
                                  controls['Exposure']['DefaultValue'],
@@ -99,26 +108,41 @@ class CameraCapture(Thread):
         self.camera.set_control_value(asi.ASI_GAIN,
                                  controls['Gain']['DefaultValue'],
                                  auto=True)
-        self.camera.set_control_value(controls['AutoExpMaxExpMS']['ControlType'], 20000)
+        self.camera.set_control_value(controls['AutoExpMaxExpMS']['ControlType'], 3000)
         self.camera.start_video_capture()
 
     def run(self):
         logger.info('Start capturing...')
+        last_timestamp = 0
         try:
             while not self.terminate:
-                logger.debug('About to take photo.')
+                if time() < last_timestamp + self.interval:
+                    sleep(0.1)
+                    continue
+                last_timestamp = time()
+                #logger.debug('About to take photo.')
                 settings = self.camera.get_control_values()
                 logger.debug('Gain {gain:d}  Exposure: {exposure:f}'.format(gain=settings['Gain'],
                           exposure=settings['Exposure']))
+                self.last_gain = settings['Gain']
+                self.last_exposure = settings['Exposure']
                 try:
-                    img = self.camera.capture_video_frame(timeout=500 + 2 * settings['Exposure'] / 1000)
+                    img = self.camera.capture_video_frame(timeout=max(1000, 500 + 2 * settings['Exposure'] / 1000))
                 except Exception as e:
                     logger.error(e)
                     self.camera.stop_exposure()
                     self.camera.stop_video_capture()
                     self.camera.close()
                     self.initialize_camera()
+                    # Set the exposure and gain to the last known good value to reduce the auto exposure time
+                    self.camera.set_control_value(asi.ASI_EXPOSURE,
+                                             self.last_exposure,
+                                             auto=True)
+                    self.camera.set_control_value(asi.ASI_GAIN,
+                                             self.last_gain,
+                                             auto=True)
                     continue
+                error_count = 0
                 # convert the numpy array to PIL image
                 mode = None
                 if len(img.shape) == 3:
@@ -140,7 +164,7 @@ class CameraCapture(Thread):
 if __name__ == '__main__':
     stream_output = StreamingOutput()
     latest_output = StreamingOutput()
-    thread = CameraCapture(stream_output, latest_output)
+    thread = CameraCapture(stream_output, latest_output, 1)
     try:
         address = ('', 8000)
         server = StreamingServer(address, StreamingHandler, stream_output, latest_output)
